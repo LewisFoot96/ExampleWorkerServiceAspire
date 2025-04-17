25a3206a6c2fc41abbb2dd5295e8a86b219877f1
Lewis Foot <lfoot91@hotmail.co.uk>
2025-04-17 08:29:13 +0100

    feat: added docker file

--- ExampleWorkerService.AppHost/Program.cs
+++ ExampleWorkerService.AppHost/Program.cs
@@ -2,6 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.ExampleWorkerService>("exampleworkerservice");
 
-builder.AddProject<Projects.ExampleWorkerServiceTwo>("exampleworkerservicetwo");
+//builder.AddProject<Projects.ExampleWorkerServiceTwo>("exampleworkerservicetwo");
+
+var container = builder.AddDockerfile(
+    "exampletwo", "../ExampleWorkerServiceTwo");
 
 builder.Build().Run();
